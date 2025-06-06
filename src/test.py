d153759486006c24e13ad806033939165214a737
Bowen <bowen.xie@infinityrobot.cn>
2025-06-06 10:27:58 +0800

    Initial commit: hello world in C++ and Python

---
+++ src/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import subprocess
+
+# Run C++ executable 3 times
+print("Running C++ program 3 times...")
+for i in range(3):
+    subprocess.run(["./cpp_demo/main"])
+
+# Run Python script 2 times
+print("Running Python script 2 times...")
+for i in range(2):
+    subprocess.run(["python3", "python_demo/hello.py"])
