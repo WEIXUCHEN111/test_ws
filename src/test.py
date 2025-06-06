9abbd277a22f094a9f2b78365d2b5e7cac24c5d0
Bowen <bowen.xie@infinityrobot.cn>
2025-06-06 11:47:14 +0800

    fix: correct path in test.py

--- src/test.py
+++ src/test.py
@@ -4,9 +4,9 @@ import subprocess
 # Run C++ executable 3 times
 print("Running C++ program 3 times...")
 for i in range(3):
-    subprocess.run(["./cpp_demo/main"])
+    subprocess.run(["src/cpp_demo/main"])
 
 # Run Python script 2 times
 print("Running Python script 2 times...")
 for i in range(2):
-    subprocess.run(["python3", "python_demo/hello.py"])
+    subprocess.run(["python3", "src/python_demo/hello.py"])
